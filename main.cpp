449c6a5234ee4aac9df8de0fe9fbc3752718a2e6
TrutnevaOV <trutneva.o@list.ru>
2019-04-02 14:33:42 +0300

    code: функция поиска минимума и максимума

--- main.cpp
+++ main.cpp
@@ -14,6 +14,24 @@ input_numbers(size_t count)
     return result;
 }
 
+void
+find_minmax(vector<double> numbers, double& min, double& max)
+{
+    min = numbers[0];
+    max = numbers[0];
+    for (double number : numbers)
+    {
+        if (number < min)
+        {
+            min = number;
+        }
+        if (number > max)
+        {
+            max = number;
+        }
+    }
+}
+
 int
 main()
 {
@@ -30,19 +48,8 @@ main()
     cin >> bin_count;
 
     // Обработка данных
-    double min = numbers[0];
-    double max = numbers[0];
-    for (double number : numbers)
-    {
-        if (number < min)
-        {
-            min = number;
-        }
-        if (number > max)
-        {
-            max = number;
-        }
-    }
+    double min, max;
+    find_minmax (numbers, min, max);
 
     vector<size_t> bins(bin_count);
     for (double number : numbers)
