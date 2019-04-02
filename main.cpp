236cec330806f10be8253bded03a11057fcbe1b6
TrutnevaOV <trutneva.o@list.ru>
2019-04-02 14:16:12 +0300

    code: функция ввода чисел

--- main.cpp
+++ main.cpp
@@ -3,18 +3,27 @@
 
 using namespace std;
 
+vector<double>
+input_numbers(size_t count)
+{
+    vector<double> result(count);
+    cerr << "Enter numbers: ";
+    for (size_t i = 0; i < count; i++)
+    {
+        cin >> result[i];
+    }
+    return result;
+}
+
 int
-main() {
+main()
+{
     // ¬вод данных
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
+    vector<double> numbers = input_numbers(number_count);
 
-    cerr << "Enter numbers: ";
-    vector<double> numbers(number_count);
-    for (size_t i = 0; i < number_count; i++) {
-        cin >> numbers[i];
-    }
 
     size_t bin_count;
     cerr << "Enter column count: ";
@@ -23,19 +32,24 @@ main() {
     // ќбработка данных
     double min = numbers[0];
     double max = numbers[0];
-    for (double number : numbers) {
-        if (number < min) {
+    for (double number : numbers)
+    {
+        if (number < min)
+        {
             min = number;
         }
-        if (number > max) {
+        if (number > max)
+        {
             max = number;
         }
     }
 
     vector<size_t> bins(bin_count);
-    for (double number : numbers) {
+    for (double number : numbers)
+    {
         size_t bin = (size_t)((number - min) / (max - min) * bin_count);
-        if (bin == bin_count) {
+        if (bin == bin_count)
+        {
             bin--;
         }
         bins[bin]++;
@@ -46,29 +60,36 @@ main() {
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
     size_t max_count = 0;
-    for (size_t count : bins) {
-        if (count > max_count) {
+    for (size_t count : bins)
+    {
+        if (count > max_count)
+        {
             max_count = count;
         }
     }
     const bool scaling_needed = max_count > MAX_ASTERISK;
 
-    for (size_t bin : bins) {
-        if (bin < 100) {
+    for (size_t bin : bins)
+    {
+        if (bin < 100)
+        {
             cout << ' ';
         }
-        if (bin < 10) {
+        if (bin < 10)
+        {
             cout << ' ';
         }
         cout << bin << "|";
 
         size_t height = bin;
-        if (scaling_needed) {
+        if (scaling_needed)
+        {
             const double scaling_factor = (double)MAX_ASTERISK / max_count;
             height = (size_t)(bin * scaling_factor);
         }
 
-        for (size_t i = 0; i < height; i++) {
+        for (size_t i = 0; i < height; i++)
+        {
             cout << '*';
         }
         cout << '\n';
