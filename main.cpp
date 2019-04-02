134ede0669bfc32b8466653c02b47ca35461c157
TrutnevaOV <trutneva.o@list.ru>
2019-04-02 15:19:34 +0300

    code: функции по построению гистограммы

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ input_numbers(size_t count)
 }
 
 void
-find_minmax(vector<double> numbers, double& min, double& max)
+find_minmax(const vector<double>& numbers, double& min, double& max)
 {
     min = numbers[0];
     max = numbers[0];
@@ -32,26 +32,12 @@ find_minmax(vector<double> numbers, double& min, double& max)
     }
 }
 
-int
-main()
+vector<size_t>
+make_histogram(const vector<double>& numbers, size_t bin_count)
 {
-    // Ввод данных
-    size_t number_count;
-    cerr << "Enter number count: ";
-    cin >> number_count;
-    cerr << "Enter numbers: ";
-    const auto numbers = input_numbers(number_count);
-
-
-    size_t bin_count;
-    cerr << "Enter column count: ";
-    cin >> bin_count;
-
-    // Обработка данных
+    vector<size_t> bins(bin_count);
     double min, max;
     find_minmax (numbers, min, max);
-
-    vector<size_t> bins(bin_count);
     for (double number : numbers)
     {
         size_t bin = (size_t)((number - min) / (max - min) * bin_count);
@@ -61,8 +47,12 @@ main()
         }
         bins[bin]++;
     }
+    return bins;
+}
 
-    // Вывод данных
+void
+show_histogram_text( vector<size_t> bins)
+{
     const size_t SCREEN_WIDTH = 80;
     const size_t MAX_ASTERISK = SCREEN_WIDTH - 4 - 1;
 
@@ -101,6 +91,27 @@ main()
         }
         cout << '\n';
     }
+
+}
+
+int
+main()
+{
+    // Ввод данных
+    size_t number_count;
+    cerr << "Enter number count: ";
+    cin >> number_count;
+    cerr << "Enter numbers: ";
+    const auto numbers = input_numbers(number_count);
+    size_t bin_count;
+    cerr << "Enter column count: ";
+    cin >> bin_count;
+
+    //Расчет количества чисел в столбцах гистограммы
+    const auto bins = make_histogram(numbers, bin_count);
+
+    //Отображение гистограммы
+    show_histogram_text(bins);
 
     return 0;
 }
