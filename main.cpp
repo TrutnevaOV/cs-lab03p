6423e577b36085d595c91cbb116819887c4f3418
TrutnevaOV <trutneva.o@list.ru>
2019-04-02 14:23:36 +0300

    code: вывод типов переменных

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ vector<double>
 input_numbers(size_t count)
 {
     vector<double> result(count);
-    cerr << "Enter numbers: ";
     for (size_t i = 0; i < count; i++)
     {
         cin >> result[i];
@@ -22,7 +21,8 @@ main()
     size_t number_count;
     cerr << "Enter number count: ";
     cin >> number_count;
-    vector<double> numbers = input_numbers(number_count);
+    cerr << "Enter numbers: ";
+    const auto numbers = input_numbers(number_count);
 
 
     size_t bin_count;
